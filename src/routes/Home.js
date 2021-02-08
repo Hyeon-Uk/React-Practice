931ce41bb58ceccc89daacb5531aaa2ae9e2cd21
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-02-08 20:47:04 +0900

    #2.6 Log Out

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,9 @@ import React from "react";
 import { authService } from "../fbase";
 
 function Home(){
-    const onClick=()=>{
-        authService.signOut();
-    }
+
     return (<div>
         <span>Home</span>
-        <button onClick={onClick}>Log Out</button>
     </div>);
 }
 
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,18 @@
 import React from "react";
-
-function Profile(){
-    return (<span>Profile</span>);
+import { useHistory } from "react-router-dom";
+import { authService } from "../fbase";
+function Profile() {
+    const history = useHistory();
+    const onClick = () => {
+        authService.signOut();
+        history.push("/");
+    }
+    return (
+        <>
+            <span>Profile</span>
+            <button onClick={onClick}>Log Out</button>
+        </>
+    );
 }
 
 export default Profile;
