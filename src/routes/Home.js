572b658a7fbf884246796178f6fc22e47dd4f406
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-02-11 20:54:24 +0900

    #3.0 Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 import { authService } from "../fbase";
 
 function Home(){
+    const [nweet,setNweet]=useState("");
 
+    const onSubmit=(event)=>{
+        event.preventDefault();
+    }
+
+    const onChange=(event)=>{
+        const {target:{value}}=event;
+        setNweet(value);
+    }
     return (<div>
-        <span>Home</span>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={nweet} type="text" maxLength="120" placeholder="What's your mind?"></input>
+            <input type="submit" value="Nweet"></input>
+        </form>
     </div>);
 }
 
