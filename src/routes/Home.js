bdd4d324618977c802f130b20799790ef1536a7a
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-02-12 02:51:04 +0900

    3.2 Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,36 @@
-import React,{useState} from "react";
-import {dbService } from "../fbase";
-function Home(){
-    const [nweet,setNweet]=useState("");
+import React, { useState, useEffect } from "react";
+import { dbService } from "../fbase";
+function Home() {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
 
-    const onSubmit=async (event)=>{
+    //이부분 다시 공부
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nwit").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
+
+    const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nwit").add({
             nweet,
-            createAt:Date.now()
+            createAt: Date.now()
         });
         setNweet("");
     }
 
-    const onChange=(event)=>{
-        const {target:{value}}=event;
+    const onChange = (event) => {
+        const { target: { value } } = event;
         setNweet(value);
     }
     return (<div>
@@ -21,6 +38,13 @@ function Home(){
             <input onChange={onChange} value={nweet} type="text" maxLength="120" placeholder="What's your mind?"></input>
             <input type="submit" value="Nweet"></input>
         </form>
+        <div>
+            {nweets.map((nweet) => (
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+            ))}
+        </div>
     </div>);
 }
 
